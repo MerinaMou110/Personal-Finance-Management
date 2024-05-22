895b2aa25de4f8d206612cacf3706fc51c8575f0
MerinaMou110 <122225168+MerinaMou110@users.noreply.github.com>
2024-05-23 01:57:34 +0600

    changes

--- personal_Finance/settings.py
+++ personal_Finance/settings.py
@@ -95,7 +95,6 @@ WSGI_APPLICATION = "personal_Finance.wsgi.application"
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': env("DB_NAME"),
         'USER': env("DB_USER"),
         'PASSWORD': env("DB_PASSWORD"),
