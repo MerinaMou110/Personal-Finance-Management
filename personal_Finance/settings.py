57bac79ee34f47e07d5f30a770b7c625efbbaa0c
MerinaMou110 <122225168+MerinaMou110@users.noreply.github.com>
2024-05-23 00:56:17 +0600

    changes

--- personal_Finance/settings.py
+++ personal_Finance/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = "django-insecure-&x=am)2iqf4n$^i-rb2oi6vahn_loo#+mkhht&0)z6r%i%2%#-"
+
+
 SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
