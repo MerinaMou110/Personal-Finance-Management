ee62cbd895e91d74ca96305c87444195eb624bda
MerinaMou110 <122225168+MerinaMou110@users.noreply.github.com>
2024-05-23 18:02:54 +0600

    add link

--- authentication/views.py
+++ authentication/views.py
@@ -31,7 +31,7 @@ class UserRegistrationApiView(APIView):
             # user er unique id toiri hoise
             uid = urlsafe_base64_encode(force_bytes(user.pk))
             print("uid ", uid)
-            confirm_link = f" http://127.0.0.1:8000/authenticate/active/{uid}/{token}"
+            confirm_link = f"https://personal-finance-management-hauf.onrender.com/authenticate/active/{uid}/{token}"
 
             email_subject = "Confirm Your Email"
             email_body = render_to_string('confirm_email.html', {'confirm_link' : confirm_link})
